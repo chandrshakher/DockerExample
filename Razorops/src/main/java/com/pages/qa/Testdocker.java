210d0b60d11bb81d53e60d1b67bd47135d4259d1
chandrashekharyadav <chandrashekharyadav@192.168.1.2>
2021-08-26 21:08:37 +0530

    First Time upload file by chandra

---
+++ Razorops/src/main/java/com/pages/qa/Testdocker.java
@@ -0,0 +1,10 @@
+package com.pages.qa;
+
+public class Testdocker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("chandra rajorops");
+	}
+
+}
